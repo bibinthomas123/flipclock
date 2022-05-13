1fdd3f97019f9a1d9e6f6e9513342d47b7cf1c78
=bibin thomas <bibinthomas951@gmail.com>
2022-05-13 12:28:17 +0530

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-const countToDate = new Date().setHours(new Date().getHours() + 24)
+const countToDate = new Date().setHours(new Date().getHours() + 24);
 
-let previousTimeBetweenDates
-setInterval(() => {
-  const currentDate = new Date()
-  
-  flipAllCards()
+let previousTimeBetweenDates;
 
-  previousTimeBetweenDates = currentDate
-}, 250)
+setInterval(() => {     
+  const currentDate = new Date();   
+
+  flipAllCards();
+  previousTimeBetweenDates = currentDate;
+}, 250);
 
 function flipAllCards() {
-  const time =new Date()
-  const seconds = time.getSeconds()
-  const minutes = time.getMinutes()
-  const hours = time.getHours()
+  const time = new Date();  //acessing the current time 
+  const seconds = time.getSeconds();   
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
 
-  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10))
-  flip(document.querySelector("[data-hours-ones]"), hours % 10)
-  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10))
-  flip(document.querySelector("[data-minutes-ones]"), minutes % 10)
-  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10))
-  flip(document.querySelector("[data-seconds-ones]"), seconds % 10)
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
 }
 
 function flip(flipCard, newNumber) {
-  const topHalf = flipCard.querySelector(".top")
-  const startNumber = parseInt(topHalf.textContent)
-  if (newNumber === startNumber) return
+  const topHalf = flipCard.querySelector(".top");
+  const startNumber = parseInt(topHalf.textContent);
+  if (newNumber === startNumber) return;
 
-  const bottomHalf = flipCard.querySelector(".bottom")
-  const topFlip = document.createElement("div")
-  topFlip.classList.add("top-flip")
-  const bottomFlip = document.createElement("div")
-  bottomFlip.classList.add("bottom-flip")
+  const bottomHalf = flipCard.querySelector(".bottom");
+  const topFlip = document.createElement("div");
+  topFlip.classList.add("top-flip");
+  const bottomFlip = document.createElement("div");
+  bottomFlip.classList.add("bottom-flip");
 
-  top.textContent = startNumber
-  bottomHalf.textContent = startNumber
-  topFlip.textContent = startNumber
-  bottomFlip.textContent = newNumber
+  top.textContent = startNumber;
+  bottomHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
+  bottomFlip.textContent = newNumber;
 
-  topFlip.addEventListener("animationstart", e => {
-    topHalf.textContent = newNumber
-  })
-  topFlip.addEventListener("animationend", e => {
-    topFlip.remove()
-  })
-  bottomFlip.addEventListener("animationend", e => {
-    bottomHalf.textContent = newNumber
-    bottomFlip.remove()
-  })
-  flipCard.append(topFlip, bottomFlip)
+  topFlip.addEventListener("animationstart", (e) => {
+    topHalf.textContent = newNumber;
+  });
+  topFlip.addEventListener("animationend", (e) => {
+    topFlip.remove();
+  });
+  bottomFlip.addEventListener("animationend", (e) => {
+    bottomHalf.textContent = newNumber;
+    bottomFlip.remove();
+  });
+  flipCard.append(topFlip, bottomFlip);
 }
